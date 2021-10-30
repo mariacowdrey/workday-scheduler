3fe99dc32b3eb83f1a366a49790f7c600e968f74
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-10-30 13:34:41 -0700

    update readme

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ function trackTime () {
         if(timeBlock < currentTime) {
             $(this).removeClass('future');
             $(this).removeClass('present');
-            $(this).addClass('past)');
+            $(this).addClass('past');
         }
         else if (timeBlock === currentTime) {
             $(this).removeClass('past');
@@ -35,6 +35,18 @@ function trackTime () {
         }
     })
 }
+
+// Get description from local storage
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
 trackTime();
 })
