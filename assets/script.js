cda5a259d9992d08027c69ede519580e76b5a509
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-10-30 13:20:45 -0700

    add time indicators

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,40 @@
 // Today's date
 var today = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(today);
+$('#currentDay').html(today);
+
+$(document).ready(function () {
+    $('.saveBtn').on('click', function () {
+        var text = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+
+        // Save in local storage
+        localStorage.setItem(time, text);
+    })
+
+function trackTime () {
+    var currentTime = moment().hour();
+
+    $('.time-block').each(function () {
+        var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
+
+        // Check time and add color indicator
+        if(timeBlock < currentTime) {
+            $(this).removeClass('future');
+            $(this).removeClass('present');
+            $(this).addClass('past)');
+        }
+        else if (timeBlock === currentTime) {
+            $(this).removeClass('past');
+            $(this).removeClass('future');
+            $(this).addClass('present');
+        }
+        else {
+            $(this).removeClass('present');
+            $(this).removeClass('past');
+            $(this).addClass('future');
+        }
+    })
+}
+
+trackTime();
+})
